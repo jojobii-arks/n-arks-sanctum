97435d4a994c7d6f31cd79ec0b01fdb58a2591fc
Joseph Bravo <josephangelobravo@gmail.com>
2022-04-27 00:05:16 +0000

    created toggle and added display for augment name

---
+++ src/routes/Augment.svelte
@@ -0,0 +1,25 @@
+<script>
+  import RightChevron from './icons/RightChevron.svelte';
+  import DownChevron from './icons/DownChevron.svelte';
+
+  let open = false;
+
+  function toggleDisplay() {
+    open = !open;
+  }
+
+</script>
+
+<li>
+  <div on:click={toggleDisplay} class="bg-cyan-900 flex justify-between items-center gap-4">
+    <h3>Augment Name</h3>
+    <h3>+BP</h3>
+
+    {#if open}
+    <DownChevron />
+    {:else}
+    <RightChevron />
+    {/if}
+
+  </div>
+</li>
--- src/routes/GearPiece.svelte
+++ src/routes/GearPiece.svelte
@@ -1,5 +1,16 @@
+<script>
+  import Augment from './Augment.svelte'
+</script>
+
 <div class="grid-flow-row text-white">
-  <div class="bg-red-900 text-right py-2">
-    <h2>Weapon</h2>
+  <div class="bg-red-900 text-right p-2 flex justify-between items-center gap-4">
+    <img src="./src/assets/icons-effect/icon-weapon.svg" alt="">
+    <h2 class="text-xl">Weapon</h2>
+  </div>
+  <div class="grid-flow-row">
+    <Augment />
+    <Augment />
+    <Augment />
+    <Augment />
   </div>
 </div>
